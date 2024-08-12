b8feeb4a8ea69495eaa7c3dded5c861d46984542
yogagv <yoganandhan465@gmail.com>
2024-08-12 08:44:38 +0530

    Hero_Changes

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,10 +1,7 @@
 import React from 'react'
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
-import Slider from "react-slick";
-import heroone from '../../assets/images/hero-one.png'
-import herotwo from '../../assets/images/hero-two.png'
-import herothree from '../../assets/images/hero_three.png'
+import heroone from '../../assets/images/hero-one.png';
 import './hero.css'
 
 const Hero = () => {
@@ -16,14 +13,12 @@ const Hero = () => {
         <div className="hero">
         <div className="container">
             <div className="row">
-            {/* <Slider {...settings}> */}
                 <div className="image-container mt-5">
                     <img src={heroone}
                       className="hero-one mt-3" alt="" />
                 <h1 className='overlay-text'>Empowering Your Workforce Needs Connecting Talent with Opportunity
             </h1>
                 </div>
-                {/* </Slider> */}
             </div>
         </div>
     </div>
